3dceb349e3c4f7c5e1af552eeba4f5111d98dc46
matval <matteo.vallar@epfl.ch>
2020-07-29 16:48:52 +0200

    improved plot and check of ascot

--- check_ascotinput.py
+++ check_ascotinput.py
@@ -3,11 +3,11 @@ Sanity checks on the input to ascot
 """
 
 from __future__ import print_function
-import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import a4py.classes.ReadEQDSK
 from plot.plot_ascotinput import plot_bstruct
+import a5py.ascot5io.ascot5 as a5                                                                                                   
 
 def check_ascotinput(fin='/home/vallar/JT60-SA/3D/bfield/003/3Dfield_forascot_onlyTF.h5'):
     """
@@ -17,14 +17,7 @@ def check_ascotinput(fin='/home/vallar/JT60-SA/3D/bfield/003/3Dfield_forascot_on
     
     @author: vallar
     """
-    f=h5py.File(fin)
-    id =  list(f['bfield'].keys())[0]
-    if '3' not in id[0:5]:
-        for i, el in enumerate(list(f['bfield'].keys())):
-            print(i,el)
-        nn = input('Print which id number you want \n')
-        id =  list(f['bfield'].keys())[int(nn)]
-    print('Id chosen is ' + id)
-    bstruct = f['bfield/'+id]
+    f=a5.Ascot(fin) 
+    bstruct = f.bfield.active.read()
     plot_bstruct(bstruct)
     return bstruct
--- plot/plot_ascotinput.py
+++ plot/plot_ascotinput.py
@@ -3,50 +3,49 @@ Sanity checks on the input to ascot
 """
 
 from __future__ import print_function
-import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-import a4py.classes.ReadEQDSK
-
+import a4py.classes.ReadEQDSK as ReadEQDSK
+import a5py.ascot5io.ascot5 as ascot5
 def plot_bstruct(bstruct):
     """
     Plots Bphi on midplane, BR and Bz at phi=0
     """
     #Creating grid
-    nR = bstruct['n_R'].value
-    Rmin, Rmax = bstruct['R_min'].value, bstruct['R_max'].value
+    nR = np.squeeze(bstruct['b_nr'][()])
+    Rmin, Rmax = np.squeeze(bstruct['b_rmin'][()]), np.squeeze(bstruct['b_rmax'][()])
     R = np.linspace(Rmin, Rmax, nR)
-    nz = bstruct['n_z'].value
-    zmin, zmax = bstruct['z_min'].value, bstruct['z_max'].value
+    nz = np.squeeze(bstruct['b_nz'][()])
+    zmin, zmax = np.squeeze(bstruct['b_zmin'][()]), np.squeeze(bstruct['b_zmax'][()])
     z = np.linspace(zmin, zmax, nz)
-    nphi = bstruct['n_phi'].value
-    phimin, phimax = bstruct['phi_min'].value, bstruct['phi_max'].value
+    nphi = np.squeeze(bstruct['b_nphi'][()])
+    phimin, phimax = np.squeeze(bstruct['b_phimin'][()]), np.squeeze(bstruct['b_phimax'][()])
     phi = np.linspace(phimin, phimax, nphi)
 
 
     # Bphi has the shape (phi, z, R)
-    Bphi = bstruct['B_phi'].value
+    Bphi = bstruct['bphi'][()]
     ind_midplane = np.argmin(z-0 <0)
     fig=plt.figure(figsize=(20,8)); 
     ax=fig.add_subplot(131); ax.set_title(r'$B_\phi$ on midplane')
-    cs=ax.contour(R, phi, Bphi[:, ind_midplane, :], 40)
+    cs=ax.contour(R, phi, Bphi[:, :, ind_midplane].T, 40)
     cb=plt.colorbar(cs)
     ax.set_xlabel(r'R [m]'); ax.set_ylabel(r'phi')
     fig.tight_layout()
 
-    BR = bstruct['B_R'].value
+    BR = bstruct['br'][()]
     ind_phi0 = np.argmin(phi-0 <0)
     ax=fig.add_subplot(132); ax.set_title(r'$B_R (\phi=0)$ ')
-    cs=ax.contour(R, z, BR[ind_phi0,:, :], 40); 
+    cs=ax.contour(R, z, BR[:,ind_phi0, :].T, 40); 
     cb=plt.colorbar(cs)
     ax.set_xlabel(r'R [m]'); ax.set_ylabel(r'z [m]')
     ax.axis('equal')
     fig.tight_layout()
 
-    Bz = bstruct['B_z'].value
+    Bz = bstruct['bz'][()]
     ind_phi0 = np.argmin(phi-0 <0)
     ax=fig.add_subplot(133); ax.set_title(r'$B_z (\phi=0)$ ')
-    cs=ax.contour(R, z, Bz[ind_phi0,:, :], 40); 
+    cs=ax.contour(R, z, Bz[:, ind_phi0, :].T, 40); 
     cb=plt.colorbar(cs)
     ax.set_xlabel(r'R [m]'); ax.set_ylabel(r'z [m]')
     ax.axis('equal')
@@ -70,57 +69,55 @@ def compare_3Dvs2D(fin_3d='/home/vallar/JT60-SA/3D/bfield/biosaw2ascot/ascot_TFf
     @author: vallar
     """
     #Read 3D file
-    f=h5py.File(fin_3d)
-    id =  list(f['bfield'].keys())[1]
-    print('Id chosen is ' + id)
-    bstruct = f['bfield/'+id]
+    a5file = ascot5.Ascot(fin_3d)
+    bstruct=a5file.bfield.active.read()
 
     #Read 2D file
-    eq=ReadEQDSK.ReadEQDSK(eq_2d)
+    eq=ReadEQDSK.ReadEQDSK(eqd_2d)
     B2D = eq.B0EXP*eq.R0EXP/eq.R_grid
 
 
     #Creating grid
-    nR = bstruct['n_R'].value
-    Rmin, Rmax = bstruct['R_min'].value, bstruct['R_max'].value
+    nR = np.squeeze(bstruct['b_nr'])
+    Rmin, Rmax = np.squeeze(bstruct['b_rmin']), np.squeeze(bstruct['b_rmax'])
     R = np.linspace(Rmin, Rmax, nR)
-    nz = bstruct['n_z'].value
-    zmin, zmax = bstruct['z_min'].value, bstruct['z_max'].value
+    nz = np.squeeze(bstruct['b_nz'])
+    zmin, zmax = np.squeeze(bstruct['b_zmin']), np.squeeze(bstruct['b_zmax'])
     z = np.linspace(zmin, zmax, nz)
-    nphi = bstruct['n_phi'].value
-    phimin, phimax = bstruct['phi_min'].value, bstruct['phi_max'].value
+    nphi = np.squeeze(bstruct['b_nphi'])
+    phimin, phimax = np.squeeze(bstruct['b_phimin']), np.squeeze(bstruct['b_phimax'])
     phi = np.linspace(phimin, phimax, nphi)
 
 
     # Bphi has the shape (phi, z, R)
-    Bphi = bstruct['B_phi'].value
+    Bphi = bstruct['bphi']
     ind_midplane = np.argmin(z-0 <0)
     fig=plt.figure(); ax=fig.add_subplot(111); ax.set_title(r'$B_\phi$ on midplane')
-    cs=ax.contour(R, phi, Bphi[:, ind_midplane, :], 40)
+    cs=ax.contour(R, phi, Bphi[:, :, ind_midplane].T, 40)
     cb=plt.colorbar(cs)
     ax.set_xlabel(r'R [m]'); ax.set_ylabel(r'phi')
     fig.tight_layout()
 
-    BR = bstruct['B_R'].value
+    BR = bstruct['br']
     ind_phi0 = np.argmin(phi-0 <0)
     fig=plt.figure(); ax=fig.add_subplot(111); ax.set_title(r'$B_R (\phi=0)$ ')
-    cs=ax.contour(R, z, BR[ind_phi0,:, :], 40); 
+    cs=ax.contour(R, z, BR[:,ind_phi0, :].T, 40); 
     cb=plt.colorbar(cs)
     ax.set_xlabel(r'R [m]'); ax.set_ylabel(r'z [m]')
     fig.tight_layout()
 
-    Bz = bstruct['B_z'].value
+    Bz = bstruct['bz']
     ind_phi0 = np.argmin(phi-0 <0)
     fig=plt.figure(); ax=fig.add_subplot(111); ax.set_title(r'$B_z (\phi=0)$ ')
-    cs=ax.contour(R, z, Bz[ind_phi0,:, :], 20);
+    cs=ax.contour(R, z, Bz[:,ind_phi0, :].T, 20);
     cb=plt.colorbar(cs)
     ax.set_xlabel(r'R [m]'); ax.set_ylabel(r'z [m]')
     fig.tight_layout()
 
     fig=plt.figure(); ax=fig.add_subplot(111); ax.set_title(r'$B_\phi (\phi=0)$ ')
-    cs = ax.contour(R, z, Bphi[ind_phi0,:,:])
+    cs = ax.contour(R, z, Bphi[:,ind_phi0,:].T)
     cb=plt.colorbar(cs)
-    ax.contour(R, z, Bphi[ind_phi0,:, :], [-1.*eq.B0EXP]);
+    ax.contour(R, z, Bphi[:, ind_phi0, :].T, [-1.*eq.B0EXP]);
     ax.plot([eq.R0EXP, eq.R0EXP],[-3., 3.])
     ax.set_xlabel(r'R [m]'); ax.set_ylabel(r'z [m]')
 
