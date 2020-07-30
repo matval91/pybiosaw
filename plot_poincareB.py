6f73391813dcf372165f1db0e57468865f1a1439
matval91 <matteo.vallar@epfl.ch>
2020-07-30 08:21:31 +0200

    started modification of the poincare plot

--- plot_poincareB.py
+++ plot_poincareB.py
@@ -2,10 +2,38 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import ReadEQDSK
-
+import a5py.ascot5io.ascot5 as a5
 col = ['k', 'r', 'b', 'g', 'y', 'm']
 
-def plot_poincareB(run, asc_fname='', eqd_fname=''):
+def plot_poincareB_h5(run='', asc_fname='', eqd_fname=''):
+    if asc_fname=='': asc_fname='ascot.h5'
+    f = a5.Ascot(asc_fname)
+    if run=='':
+    	run=f.active
+    mlpol=f['results/run-'+str(run)+'/orbits/mlpol0']
+    r=mlpol['R'].value; z=mlpol['z'].value; idm=mlpol['id'].value
+
+    fig=plt.figure(); ax=fig.add_subplot(111) 
+    c=ax.scatter(r,z, c=mlpol['B_phi'], marker='.'); ax.axis('equal'); 
+    plt.colorbar(c)
+    ax.set_xlabel(r'R[m]'); ax.set_ylabel(r'Z [m]')
+    if eqd_fname!='':
+        _eq=ReadEQDSK.ReadEQDSK(eqd_fname)
+        plt.plot(_eq.R, _eq.Z, 'k-')
+
+    mltor=f['results/run-'+str(run)+'/orbits/mltor0']
+    rho = mltor['rho']; phi=np.mod(mltor['phi'], 360)
+    fig=plt.figure(); ax=fig.add_subplot(111)
+    fig.suptitle('Poincare plot on the midplane')
+    ax.scatter(rho, phi, marker='.');
+    ax.set_xlabel(r'$\rho$'); ax.set_ylabel(r'$\phi$ [°]')
+    plt.show()
+
+
+
+
+
+def plot_poincareB(run='', asc_fname='', eqd_fname=''):
     if asc_fname=='': asc_fname='ascot.h5'
     f = h5py.File(asc_fname)
     mlpol=f['results/run-'+str(run)+'/orbits/mlpol0']
