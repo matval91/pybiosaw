d313ae65e5c17e3c84caef49426a91adcce840ac
matval91 <matteo.vallar@epfl.ch>
2020-07-30 19:28:23 +0200

    Added possibility to choose for pncrid

--- plot_poincareB.py
+++ plot_poincareB.py
@@ -7,14 +7,14 @@ import utils.plot_utils as pu
 pu.common_style()
 col = ['k', 'r', 'b', 'g', 'y', 'm']
 
-def plot_poincareB(run='', asc_fname='', eqd_fname=''):
+def plot_poincareB(asc_fname='',run='', pncrid=0):
     if asc_fname=='': asc_fname='ascot.h5'
     f = a5.Ascot(asc_fname)
     if run=='': run=f.active
 
- 	#plot in rho and theta
+    #plot in rho and theta
     fig=plt.figure(); ax=fig.add_subplot(111) 
-    run.orbit.poincare('rho', 'thetamod', 0, markersize=2, axes=ax)
+    run.orbit.poincare('rho', 'thetamod', pncrid, markersize=2, axes=ax)
     ax.set_xlabel(r'$\rho_{POL}$'); ax.set_ylabel(r'$\theta$ [°]')
     fig.tight_layout()
 
@@ -23,7 +23,7 @@ def plot_poincareB(run='', asc_fname='', eqd_fname=''):
     run.orbit.poincare('R', 'z', 0, markersize=2, axes=ax)
     b5 = Ascotpy(asc_fname)
     b5.init(bfield=f.bfield.active.get_qid())
-    b5.plotseparatrix(np.linspace(0,5,100), 0, np.linspace(-3,3,100), 0, axes=ax)
+    b5.plotseparatrix(np.linspace(0,5,100), pncrid, np.linspace(-3,3,100), 0, axes=ax)
     ax.set_xlabel(r'R[m]'); ax.set_ylabel(r'Z [m]')
     fig.tight_layout()
 
