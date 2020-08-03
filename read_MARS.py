7f384152313017bc0cfbd63adee25b7d02bdd4fe
matval91 <matteo.vallar@epfl.ch>
2020-08-03 16:54:22 +0200

    Fixed dimensions;

--- read_MARS.py
+++ read_MARS.py
@@ -5,6 +5,7 @@ import h5py
 import numpy as np
 
 def read_data(fnfield, real=False):
+    print(real)
     # Read magnetic field grid, this is same for all coils
     bfield_file = h5py.File(fnfield, "r")
 	#['Bphi', 'Br', 'Bz', 'R', 'Z', 'phi']
@@ -22,14 +23,14 @@ def read_data(fnfield, real=False):
     # Note phimin=0 and phimax = 2*pi so B(phi=phimin) = B(phi=phimax) in input data
     # The last grid point where the data is given is not phimax, but it is the point just before. fucking periodicity
     data["phimin"] = bfield_file["phi"][0]*180./np.pi
-    data["phimax"] = bfield_file["phi"][-2]*180./np.pi
-    data["nphi"] = len(bfield_file["phi"][:]) - 1
+    data["phimax"] = bfield_file["phi"][-1]*180./np.pi
+    data["nphi"] = len(bfield_file["phi"][:])
     
     # Empty grid for storing the magnetic field (remove last dublicate phi point)
     # The last grid point where the data is given is not phimax, but it is the point just before. fucking periodicity
-    BR   = np.transpose(bfield_file["Br"][:,:,:-1], [1,2,0])
-    Bphi = np.transpose(bfield_file["Bphi"][:,:,:-1], [1,2,0])
-    Bz   = np.transpose(bfield_file["Bz"][:,:,:-1], [1,2,0])
+    BR   = np.transpose(bfield_file["Br"][:,:,:], [1,2,0])
+    Bphi = np.transpose(bfield_file["Bphi"][:,:,:], [1,2,0])
+    Bz   = np.transpose(bfield_file["Bz"][:,:,:], [1,2,0])
     if real:
     	BR = np.real(BR); Bphi=np.real(Bphi); Bz = np.real(Bz)
     else:
